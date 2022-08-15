1d8397440f3a2f4641870d4859ac0e73bd7bbcbb
Kevin <kelott@gmx.de>
2022-08-15 15:27:56 +0200

    connect server to database

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { corsOptions, port, host } from './utils/config';
 import router from './router';
+import { connectDb } from './models';
 
 const app = express();
 
@@ -11,6 +11,13 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at ${host}:${port}`);
-});
+(async () => {
+  try {
+    await connectDb();
+    app.listen(port, () => {
+      console.log(`⚡️[server]: Server is running at ${host}:${port}`);
+    });
+  } catch (e) {
+    console.log('⚡️[server]: Server could not connect to database!');
+  }
+})();
