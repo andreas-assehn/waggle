1a2a6aa61cc06283ee65796202ad346363f56928
Andreas Asséhn <andreas.assehn@gmail.com>
2022-08-15 20:46:15 +0200

    set up example is in SplashScreen.tsx

--- client/src/pages/SplashScreen.tsx
+++ client/src/pages/SplashScreen.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import { buildUrl } from 'cloudinary-build-url';
+
 function SplashScreen() {
+  const idFromDB = 73829173683254;
+  const cld = buildUrl(`/waggle/${idFromDB}`, {
+    cloud: {
+      cloudName: process.env.REACT_APP_CLOUDINARY_URL,
+    },
+  });
+
   return (
     <>
       <div>SplashScreen</div>
+      <img src={cld} />
     </>
   );
 }
