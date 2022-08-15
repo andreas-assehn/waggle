5b1c19aa8408752eae95633df19b2139f0f848b5
Kevin <kelott@gmx.de>
2022-08-15 16:01:25 +0200

    gitignore: ignore files in dist folder

--- server/dist/utils/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.host = exports.port = exports.corsOptions = void 0;
-exports.corsOptions = {
-    origin: 'http://localhost:3000',
-};
-exports.port = 4000;
-exports.host = 'http://localhost';
