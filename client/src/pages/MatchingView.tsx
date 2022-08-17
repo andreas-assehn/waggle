64425a241b5d89ccfb57d2ac32aaafe27321f697
Kevin <kelott@gmx.de>
2022-08-17 20:09:43 +0200

    fix type issues

--- client/src/pages/MatchingView.tsx
+++ client/src/pages/MatchingView.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+// import SwipeBar from '../features/SwipeBar';
+import TinderCard from 'brian-react-tinder-card';
+import { RootState } from '../app/store';
+import { User } from '../../../globalUtils/Types';
+import { useAppSelector } from '../app/hooks';
 
 function MatchingView() {
-  return (
+  const { allUsers } = useAppSelector((state: RootState) => state.allUsers);
+
+  const onSwipe = (direction: string) => {
+    console.log('You swiped: ' + direction);
+  };
+
+  const onCardLeftScreen = (myIdentifier: string) => {
+    console.log(myIdentifier + ' left the screen');
+  };
+
+  return allUsers && allUsers.length ? (
     <>
-      <div>MatchingView</div>
+      {allUsers.map((user: User) => {
+        return (
+          <TinderCard
+            key={user._id}
+            onSwipe={onSwipe}
+            onCardLeftScreen={() => onCardLeftScreen('fooBar')}
+            preventSwipe={['right', 'left']}
+          >
+            Hello, World!
+            <p>{user.name}</p>
+          </TinderCard>
+        );
+      })}
     </>
+  ) : (
+    <div>Loading</div>
   );
 }
 
