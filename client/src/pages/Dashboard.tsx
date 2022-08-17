036903bc913104d3d08724d655c9c493028ca8a7
Andreas Asséhn <andreas.assehn@gmail.com>
2022-08-17 20:01:39 +0200

    added some sass

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import BurgerMenu from '../components/BurgerMenu';
 
 function Dashboard() {
   return (
-    <>
+    <div style={{ backgroundColor: 'grey' }}>
       <div>Dashboard</div>
-    </>
+      <BurgerMenu />
+    </div>
   );
 }
 
