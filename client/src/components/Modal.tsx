63b62e9936513f1a032ce2d07c8ccc51be27ac20
Kevin <kelott@gmx.de>
2022-08-18 17:10:40 +0200

    basic styling

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import '../Css/components/Modal.css';
 
 function Modal({
   setOpenModal,
   message,
   handleModalConfirm,
   openModal,
+  enableCross = true,
 }: {
   setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
   message: string;
   handleModalConfirm: any;
   openModal: boolean;
+  enableCross: boolean;
 }) {
   if (!openModal) return null;
   // To use this modal, copy the following useState into the page you are calling the modal from:
@@ -28,29 +31,31 @@ function Modal({
   // }
 
   return (
-    <div className='modalBackground'>
-      <div className='modalContainer'>
-        <div className='titleCloseBtn'>
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-          >
-            X
-          </button>
-        </div>
-        <div className='modalBody'>
+    <div className="modalBackground">
+      <div className="modalContainer">
+        {enableCross && (
+          <div className="titleCloseBtn">
+            <button
+              onClick={() => {
+                setOpenModal(false);
+              }}
+            >
+              X
+            </button>
+          </div>
+        )}
+        <div className="modalBody">
           <p>{message}</p>
         </div>
-        <div className='modalFooter'>
-          <button onClick={handleModalConfirm} id='modalConfirmBtn'>
+        <div className="modalFooter">
+          <button onClick={handleModalConfirm} id="modalConfirmBtn">
             Continue
           </button>
           <button
             onClick={() => {
               setOpenModal(false);
             }}
-            id='modalCancelBtn'
+            id="modalCancelBtn"
           >
             Cancel
           </button>
--- client/src/pages/ChatDashboard.tsx
+++ client/src/pages/ChatDashboard.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '../components/Modal';
 
 function ChatDashboard() {
+  const [openModal, setOpenModal] = useState(false);
+  const message =
+    'Are you sure you want to delete all messages with Guapa? This action is irreversible.';
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleModalConfirm = () => {
+    // whatever your function wants to do on confirm
+  };
+
   return (
     <>
       <div>ChatDashboard</div>
+      <button onClick={handleOpenModal}>Open Modal</button>
+      <Modal
+        setOpenModal={setOpenModal}
+        message={message}
+        handleModalConfirm={handleModalConfirm}
+        openModal={openModal}
+        enableCross={true}
+      />
     </>
   );
 }
