1ee24270dd479508eaf896a02fd837c9fe3f9e36
Keval <keval.p21+github@gmail.com>
2022-08-18 08:51:07 +0100

    modal done

---
+++ client/src/components/Modal.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+function Modal({
+  setOpenModal,
+  message,
+  title,
+  handleModalConfirm,
+  openModal,
+}: {
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+  message: string;
+  title: string;
+  handleModalConfirm: any;
+  openModal: boolean;
+}) {
+  if (!openModal) return null;
+  // To use this modal, copy the following useState into the page you are calling the modal from:
+  // const [openModal, setOpenModal] = useState(false)
+
+  // Then create a function/click event to open the modal:
+  // const handleOpenModal = ()=>{
+  // setOpenModal(true)
+  // }
+
+  // the cancel and close button are already set to close the modal
+
+  // create a function for handleModalConfirm:
+  // const handleModalConfirm = ()=>{
+  // // whatever your function wants to do on confirm
+  // }
+  //
+  //
+
+  return (
+    <div className='modalBackground'>
+      <div className='modalContainer'>
+        <div className='titleCloseBtn'>
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            X
+          </button>
+        </div>
+        <div className='modalTitle'>
+          <h3>{title}</h3>
+        </div>
+        <div className='modalBody'>
+          <p>{message}</p>
+        </div>
+        <div className='modalFooter'>
+          <button onClick={handleModalConfirm} id='modalConfirmBtn'>
+            Continue
+          </button>
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+            id='modalCancelBtn'
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
