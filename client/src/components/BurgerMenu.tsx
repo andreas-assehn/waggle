e6d28050621cccb59eff28a2ec42520602744674
Andreas Asséhn <andreas.assehn@gmail.com>
2022-08-18 18:23:01 +0200

    modal for burgerMenu added

--- client/src/components/BurgerMenu.tsx
+++ client/src/components/BurgerMenu.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import burgerMenuIcon from '../assets/burgerMenu.svg';
 
 function BurgerMenu() {
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(!showModal);
+  };
+
+  // Add aria! chrome vox to test blind user experience
   return (
     <>
-      <button>
+      <button onClick={handleShowModal}>
         <img src={burgerMenuIcon} />
       </button>
+      {showModal ? (
+        <div className="modalBackground">
+          <div className="modalContainer">
+            <div className="titleCloseBtn">
+              <button
+                onClick={() => {
+                  setShowModal(false);
+                }}
+              >
+                X
+              </button>
+            </div>
+            <div className="modalBody">
+              <button
+                onClick={() => {
+                  navigate('/settingsView'), setShowModal(false);
+                }}
+              >
+                Settings
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
