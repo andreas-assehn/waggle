dc14cfc0ac07a925904c3e1f0a4ac305e865f534
Kevin <kelott@gmx.de>
2022-08-19 16:44:20 +0200

    added react-slick lib

---
+++ client/src/components/MatchPreferences.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//form
+//slider with kilometer 1-10
+
+function MatchPreferences() {
+  return (
+    <>
+      <h1>MatchPreferences</h1>
+    </>
+  );
+}
+
+export default MatchPreferences;
