d0f5d96ba5abaea53bebd76648d4b581beb8514c
Keval <keval.p21+github@gmail.com>
2022-08-19 08:24:37 +0100

    events data on events dashboard

--- client/src/pages/EventsDashboard.tsx
+++ client/src/pages/EventsDashboard.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Event } from '../../../globalUtils/Types';
+import { useAppSelector } from '../app/hooks';
+import { RootState } from '../app/store';
 
 function EventsDashboard() {
-  return (
+  const { allEvents } = useAppSelector((state: RootState) => state.allEvents);
+  return allEvents && allEvents.length ? (
     <>
       <div>EventsDashboard</div>
+      <div>
+        {allEvents.map((eventData: Event) => (
+          <div key={eventData._id}>
+            <p>{eventData.description}</p>
+            <p>{`${eventData.dateTime}`}</p>
+          </div>
+        ))}
+      </div>
     </>
+  ) : (
+    <div>Loading</div>
   );
 }
 
