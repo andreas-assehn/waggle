6ef7b6398ad46181b57b912a991ebd88cdc698a6
Freya Caudwell <freyacaudwell@gail.com>
2022-08-20 14:42:07 +0100

    typescript fix

---
+++ client/src/utils/helperFunctions/geoapifyInput.ts
@@ -0,0 +1,29 @@
+import { GeocoderAutocomplete } from '@geoapify/geocoder-autocomplete';
+import { MutableRefObject } from 'react';
+
+export const geoapifyInput = (
+  initialized: MutableRefObject<boolean>,
+  geocoderContainer: MutableRefObject<null>,
+  location: any,
+  geocoderOnSelectLogic: any
+) => {
+  if (
+    !initialized.current &&
+    process.env.REACT_APP_GEOAPIFY_KEY &&
+    geocoderContainer.current
+  ) {
+    const autocomplete = new GeocoderAutocomplete(
+      geocoderContainer.current,
+      process.env.REACT_APP_GEOAPIFY_KEY,
+      {
+        placeholder: location?.formatted || 'enter location',
+        skipDetails: false,
+        skipIcons: true,
+      }
+    );
+    autocomplete.on('select', (location) => {
+      geocoderOnSelectLogic(location);
+    });
+    initialized.current = true;
+  }
+};
