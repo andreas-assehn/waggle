635fa9dba4e01ebcd372fe12f711feb2e929f273
Kevin <kelott@gmx.de>
2022-08-20 15:41:27 +0200

    style changes

--- client/src/components/YourMatches.tsx
+++ client/src/components/YourMatches.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { User } from '../../../globalUtils/Types';
 import '../Css/components/YourMatches.css';
+import { users } from '../assets/dataUser';
 
 export default function YourMatches({
   user,
@@ -9,20 +10,37 @@ export default function YourMatches({
   user: User | null;
   matchedUsers: User[] | null;
 }) {
+  const mockMatchedUsers = users.filter(
+    (mockUser) => mockUser.userId !== user?.userId
+  );
+  // console.log(mockMatchedUsers);
+
   return (
-    <>
-      {user && matchedUsers ? (
+    <div className="your-matches">
+      {user && mockMatchedUsers ? (
         <div className="your-matches__background">
-          <div className="your-matches">
+          <div>
             <div className="your-matches__text">
-              <p>Your matches ({matchedUsers.length})</p>
+              <p>Your matches ({mockMatchedUsers.length})</p>
             </div>
-            <div className="your-matches__bubbles"></div>
+            <div className="your-matches__bubbles">
+              {mockMatchedUsers.length ? (
+                mockMatchedUsers.map((matchedUser) =>
+                  matchedUser.dog.images!.map((image) => (
+                    <div key={image}>
+                      <img src={image} />
+                    </div>
+                  ))
+                )
+              ) : (
+                <p>No matches yet</p>
+              )}
+            </div>
           </div>
         </div>
       ) : (
         <p>Loading..</p>
       )}
-    </>
+    </div>
   );
 }
