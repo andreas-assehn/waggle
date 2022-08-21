b675b796ffcf70249e05ba9b7b51425bb5ca9786
Andreas Asséhn <andreas.assehn@gmail.com>
2022-08-21 16:19:57 +0200

    more setup

--- server/index.ts
+++ server/index.ts
@@ -4,8 +4,35 @@ import cors from 'cors';
 import { corsOptions, port, host } from './utils/config';
 import router from './router';
 import { connectDb } from './models';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: corsOptions,
+});
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+});
+
+io.use((socket, next) => {
+  const userName = socket.handshake.auth.userName;
+  socket.userName = userName;
+  next();
+});
+
+io.on('connection', (socket) => {
+  const users = [];
+  for (let [id, socket] of io.of('/').sockets) {
+    users.push({
+      userID: id,
+      username: socket.userName,
+    });
+  }
+  socket.emit('users', users);
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -14,7 +41,7 @@ app.use(router);
 (async () => {
   try {
     await connectDb();
-    app.listen(port, () => {
+    httpServer.listen(port, () => {
       console.log(`⚡️[server]: Server is running at ${host}:${port}`);
     });
   } catch (e) {
