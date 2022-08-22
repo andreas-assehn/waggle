07508bdc7e946b76f17aab73d8ef2dedf684c0fe
Kevin <kelott@gmx.de>
2022-08-22 18:32:30 +0200

    re-design navbar for accessability

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import matches from '../assets/navbar-matches.svg';
-import dashboard from '../assets/navbar-dashboard.svg';
 import events from '../assets/navbar-events.svg';
 import chat from '../assets/navbar-chat.svg';
+import profile from '../assets/navbar-profile.svg';
 import '../Css/components/Navbar.css';
 import { useSelector } from 'react-redux';
 import { RootState } from '../app/store';
 
 function Navbar() {
-  const isLoggedIn = useSelector((state: RootState) => state.userAuth);
+  const userAuth = useSelector((state: RootState) => state.userAuth);
   const url = useLocation().pathname;
   const notProtected =
     url !== '/' &&
@@ -17,36 +17,66 @@ function Navbar() {
     url !== '/register' &&
     url !== '/loginRegister';
 
-  return isLoggedIn && notProtected ? (
+  return userAuth && notProtected ? (
     <>
-      <div className="navbar-padding" />
-      <div className="navbar">
-        <div className="btn-container">
-          <Link
-            className={url === '/matchingView' ? 'nav-btn current' : 'nav-btn'}
-            to={'/matchingView'}
-          >
-            <img src={matches} />
+      <div className='navbar-padding' />
+      <div className='navbar'>
+        <div className='btn-container'>
+          <Link className='nav-btn' to={'/matchingView'}>
+            <div
+              className={
+                url === '/matchingView'
+                  ? 'navbar__icon-background --current'
+                  : 'navbar__icon-background'
+              }
+            >
+              <img src={matches} />
+            </div>
+            <div>
+              <p>Matches</p>
+            </div>
           </Link>
-          <Link
-            className={url === '/dashboard' ? 'nav-btn current' : 'nav-btn'}
-            to={'/dashboard'}
-          >
-            <img src={dashboard} />
+          <Link className='nav-btn' to={'/profile'}>
+            <div
+              className={
+                url === '/profile'
+                  ? 'navbar__icon-background --current'
+                  : 'navbar__icon-background'
+              }
+            >
+              <img src={profile} />
+            </div>
+            <div>
+              <p>Profile</p>
+            </div>
           </Link>
-          <Link
-            className={
-              url === '/eventsDashboard' ? 'nav-btn current' : 'nav-btn'
-            }
-            to={'/eventsDashboard'}
-          >
-            <img src={events} />
+          <Link className='nav-btn' to={'/eventsDashboard'}>
+            <div
+              className={
+                url === '/eventsDashboard'
+                  ? 'navbar__icon-background --current'
+                  : 'navbar__icon-background'
+              }
+            >
+              <img src={events} />
+            </div>
+            <div>
+              <p>Events</p>
+            </div>
           </Link>
-          <Link
-            className={url === '/chatDashboard' ? 'nav-btn current' : 'nav-btn'}
-            to={'/chatDashboard'}
-          >
-            <img src={chat} />
+          <Link className='nav-btn' to={'/chatDashboard'}>
+            <div
+              className={
+                url === '/chatDashboard'
+                  ? 'navbar__icon-background --current'
+                  : 'navbar__icon-background'
+              }
+            >
+              <img src={chat} />
+            </div>
+            <div>
+              <p>Chat</p>
+            </div>
           </Link>
         </div>
       </div>
