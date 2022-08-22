73b3b7cf8e848009a674bf0ae20199f1fe39103a
Freya Caudwell <freyacaudwell@gail.com>
2022-08-22 19:23:53 +0100

    event dashboard rendering

--- client/src/pages/EventsDashboard.tsx
+++ client/src/pages/EventsDashboard.tsx
@@ -7,51 +7,55 @@ import EventCard from '../components/EventCard';
 import Loading from '../components/Loading';
 import '../Css/pages/EventsDashboard.css';
 import { Attendee } from '../utils/types/event';
+import { useNavigate } from 'react-router-dom';
 
 function EventsDashboard() {
-  // const [openModal, setOpenModal] = useState(false);
   const { userAuth } = useSelector((state: RootState) => state.userAuth);
   const { allEvents } = useAppSelector((state: RootState) => state.allEvents);
   const [attendingEvents, setAttendingEvents] = useState([] as Event[]);
   const [nonAttendingEvents, setNonAttendingEvents] = useState([] as Event[]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (allEvents![0].attendees!.length && userAuth!._id) {
+    if (allEvents.length && userAuth!.userId) {
       setAttendingEvents(
         allEvents.filter((eventObj: Event) => {
-          return eventObj.attendees!.filter(
-            (attendee: Attendee) => attendee.userId === userAuth!._id
-          );
+          return !!eventObj.attendees!.filter((attendee: Attendee) => {
+            console.log(userAuth!.userId, attendee.userId);
+            return attendee.userId === userAuth!.userId;
+          }).length;
         })
       );
       setNonAttendingEvents(
         allEvents.filter((eventObj) => {
-          return eventObj.attendees!.filter(
-            (attendee: Attendee) => attendee.userId !== userAuth!._id
-          );
+          return !eventObj.attendees!.filter(
+            (attendee: Attendee) => attendee.userId === userAuth!.userId
+          ).length;
         })
       );
     }
   }, [allEvents, userAuth]);
-  //TODO to test
-
-  // const handleOpenModal = () => {
-  //   setOpenModal(true);
-  // };
-
-  // const handleModalConfirm = () => {
-  //   // whatever your function wants to do on confirm
-  // };
 
   return allEvents && allEvents.length ? (
     <div className='events-dashboard'>
-      <div className='events-dashboard'>
-        {allEvents.map((eventData: Event) => (
+      <div className='events-dashboard__attending'>
+        <h2 className='events-dashboard__attending__title'>Attending</h2>
+        {attendingEvents.map((eventData: Event) => (
+          <EventCard key={eventData._id} event={eventData} />
+        ))}
+      </div>
+      <div className='events-dashboard__not-attending'>
+        {nonAttendingEvents.map((eventData: Event) => (
           <EventCard key={eventData._id} event={eventData} />
         ))}
       </div>
       <div className='events-dashboard__add-events'>
-        <button className='--round --pop'>+</button>
+        <button
+          className='--round --pop'
+          onClick={() => navigate('/addEventForm')}
+        >
+          +
+        </button>
       </div>
       <div className='events-dashboard__add-events-padding'></div>
     </div>
