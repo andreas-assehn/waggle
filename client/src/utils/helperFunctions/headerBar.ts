7c43d4a98ce65ca3c3c059b35e69db551f0d1a18
Kevin <kelott@gmx.de>
2022-08-23 09:52:36 +0200

    fix header bar not rendering on pages with url parameters

--- client/src/utils/helperFunctions/headerBar.ts
+++ client/src/utils/helperFunctions/headerBar.ts
@@ -5,9 +5,9 @@ export const pageTitle = (path: string) => {
   if (path === '/chatDashboard') return 'Matches';
   if (path === '/chat') return 'Fix this!'; // Get user from somewhere!
   if (path === '/eventsDashboard') return 'Events';
-  if (path === '/eventDetails') return 'Events';
+  if (path.includes('/eventDetails')) return 'Events';
   if (path === '/addEventForm') return 'Add Event';
-  if (path === '/editEvent') return 'Edit Event';
+  if (path.includes('/editEvent')) return 'Edit Event';
   if (path === '/settingsView') return 'Settings';
   if (path === '/editProfile') return 'Edit Profile';
   if (path === '/profile') return 'Your Profile';
@@ -17,7 +17,7 @@ export const pageTitle = (path: string) => {
 export const pagesWithBackButton = (path: string) => {
   if (path.includes('/matchingViewDetail')) return true;
   if (path === '/chat') return true;
-  if (path === '/eventDetails') return true;
+  if (path.includes('/eventDetails')) return true;
   if (path === '/addEventForm') return true;
   if (path === '/editProfile') return true;
   if (path === '/profile') return true;
